8c56ffd2a465d6c59209c0685c578b2eed41b112
paz252 <70439114+paz252@users.noreply.github.com>
2020-10-21 12:32:15 +0530

    Add files via upload

---
+++ food.js
@@ -0,0 +1,30 @@
+import {onSnake, expandSnake} from './snake.js'
+import {randomGridPosition} from './grid.js'
+
+let food = getRandomFoodPosition();
+const EXPANSION_RATE = 1;
+
+
+export function update() {
+    if(onSnake(food)){
+        expandSnake(EXPANSION_RATE);
+        food = getRandomFoodPosition();
+    }
+}
+
+export function draw(gameBoard) {
+        const foodEl = document.createElement('div');
+        foodEl.style.gridRowStart = food.y;
+        foodEl.style.gridColumnStart = food.x;
+        foodEl.classList.add('food');
+        gameBoard.appendChild(foodEl);
+}
+
+function getRandomFoodPosition(){
+    let newFoodPosition;
+    while(newFoodPosition == null || onSnake(newFoodPosition)){
+        newFoodPosition = randomGridPosition();
+    }
+    return newFoodPosition;
+}
+
